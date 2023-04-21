6585acdde148533942cdddd6c8683bfafba62e1a
GonzaloEspinoza <128237859+KingDmnT@users.noreply.github.com>
2023-04-21 19:26:17 -0400

    separar el js del index

---
+++ javas.js
@@ -0,0 +1,30 @@
+<script>
+var expr = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/;
+
+$(document).ready(function(){        
+    $("#bEnviar").click(function(){
+        var nombre = $("#itNombre").val();
+        var correo= $("#itMail").val();
+        var asunto = $("#itAsunto").val();
+
+        if(nombre == ""){
+            $("#mensaje1").fadeIn();
+            return false;
+        }else{
+            $("#mensaje1").fadeOut();
+            if(correo == "" || !expr.test(correo)){
+                $("#mensaje2").fadeIn("");
+                return true;
+            }else{
+                $("#mensaje2").fadeOut();
+                if(asunto == ""){
+                    $("#mensaje3").fadeIn("");
+                    return true;
+                }else{
+                    $("#mensaje3").fadeOut();
+                }
+            }
+        }
+    });
+    });
+</script>
